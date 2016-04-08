48cbf0f82f500da17662f98cb9612e5fefdfbacf
jespernilssons <jesper.nilsson@simplygon.com>
2016-04-08 14:13:37 +0200

    Initial Version

---
+++ GitJenkinsTest/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitJenkinsTest
+{
+    public class Player
+    {
+        public int currentHitpoints { get; private set; }
+
+        public Player(int hitpoints)
+        {
+            currentHitpoints = hitpoints;
+        }
+
+        public void receiveDamage(int hitpointsOfDamage)
+        {
+            if (hitpointsOfDamage < 0)
+            {
+                return;
+            }
+            currentHitpoints = (currentHitpoints - hitpointsOfDamage);
+
+            if (currentHitpoints < 0)
+            {
+                currentHitpoints = 0;
+            }
+        }
+    }
+}
